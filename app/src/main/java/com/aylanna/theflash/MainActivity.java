db8f7c87462748a948a946445b4cfc057a143d01
Aylanna Lopez <aylanna216@gmail.com>
2020-02-21 18:47:41 -0500

    Initial commit

---
+++ app/src/main/java/com/aylanna/theflash/MainActivity.java
@@ -0,0 +1,31 @@
+package com.aylanna.theflash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
+
+
+
+}
