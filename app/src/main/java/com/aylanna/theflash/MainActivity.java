e97b27d07b508c85d1335ca5c679f314b76b90b2
Aylanna Lopez <aylanna216@gmail.com>
2020-03-13 20:57:22 -0400

    lab 3 - add flashcards

--- app/src/main/java/com/aylanna/theflash/MainActivity.java
+++ app/src/main/java/com/aylanna/theflash/MainActivity.java
@@ -6,15 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 
 public class MainActivity extends Activity {
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +41,25 @@ public class MainActivity extends Activity {
             }
         });
 
+        findViewById(R.id.nextIcon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+
+            }
+        });
+
     }
 
 
@@ -45,9 +73,13 @@ public class MainActivity extends Activity {
                 ((TextView) findViewById(R.id.flashcard_answer)).setText(stringA);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+
+                flashcardDatabase.insertCard(new Flashcard(stringQ, stringA));
+                allFlashcards = flashcardDatabase.getAllCards();
             }
         }
     }
 
+    FlashcardDatabase flashcardDatabase;
 
 }
